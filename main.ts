6fa15a57f1c89b6e3628c697c029aa8056b9740e
syedaUroojFatimaa <162017553+syedaUroojFatimaa@users.noreply.github.com>
2024-03-15 18:40:18 +0500

    TYPESCRIPT
    exercise 45 Questions

--- main.ts
+++ main.ts
@@ -1,2 +1,9 @@
-let personName: string= "ERIC";
-console.log(`hello ${personName} would you like to learn some python today?`);
+// lower case
+let personName: string ="Urooj"
+console.log("lowercase:", personName.toLowerCase());
+
+// upper case 
+console.log("uppercase:", personName.toLocaleUpperCase());
+
+// title case
+console.log("titlecase:", personName.replace(/\bw/g,c => c.toUpperCase()));
